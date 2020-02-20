fba92c2e7e54a90c83c69ae914c26316c706aa25
xzavadska <anastasiya.zavad.az@gmail.com>
2020-02-20 22:42:52 +0100

    add file

---
+++ src/components/MenuMobileSide.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="sidebar">
+    <div class="sidebar-backdrop" @click="closeSidebarPanel" v-if="isPanelOpen"></div>
+    <transition name="slide">
+      <div v-if="isPanelOpen"
+           class="sidebar-panel">
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+<script>
+  import { store, mutations } from '@/store.js'
+
+  export default {
+    methods: {
+      closeSidebarPanel: mutations.toggleNav
+    },
+    computed: {
+      isPanelOpen () {
+        return store.isNavOpen
+      }
+    }
+  }
+</script>
+<style>
+  .sidebar-backdrop {
+    background-color: gray;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    cursor: pointer;
+  }
+
+  .sidebar-panel {
+    overflow-y: auto;
+    background-color: #ffffff;
+    position: fixed;
+    left: 0;
+    top: 0;
+    height: 100vh;
+    z-index: 999;
+    width: 280px;
+  }
+</style>
